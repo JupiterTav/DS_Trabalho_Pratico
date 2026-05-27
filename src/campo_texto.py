cb518c46f6300f009bd4d9f37d9637a657b2ff86
JupiterTav <wesleytavaresdev@outlook.com>
2026-05-27 19:30:34 -0300

    get_texto -> le_texto

---
+++ src/campo_texto.py
@@ -0,0 +1,19 @@
+# TODO: Integrar com a UI
+
+class CampoTexto:
+    def __init__(self):
+        self.linhas = []
+        print("Insira  o texto: ")
+
+    def le_texto(self):
+        lendo = True
+        while lendo:
+            linha = input()
+            if linha == "":
+                lendo = False
+            else:
+                self.linhas.append(linha)
+
+    @property
+    def Linhas(self):
+        return self.linhas
--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
-#TODO: classe campo_texto para leitura do texto (linhas) do usuario (posteriormente parte da GUI)
-#TODO: classe mixer (GerenciadorDeMusica) -> chama gerenciador_vozes e gerenciador_midi além de outras tarefas operacionais
-#TODO: Escrever a classe track (antiga voz) 
-#TODO:  - Parametros e metodos associados aos parametros
-#TODO:  - Metodos associados as tracks do midi (tocar nota, mudar instrumento, silencio) 
+# TODO: classe campo_texto para leitura do texto (linhas) do usuario (posteriormente parte da GUI)
+# TODO: classe mixer (GerenciadorDeMusica) -> chama gerenciador_vozes e gerenciador_midi além de outras tarefas operacionais
+# TODO: Escrever a classe track (antiga voz)
+# TODO:  - Parametros e metodos associados aos parametros
+# TODO:  - Metodos associados as tracks do midi (tocar nota, mudar instrumento, silencio)
+
+from campo_texto import CampoTexto
+
+texto = CampoTexto()
+texto.le_texto()
+
