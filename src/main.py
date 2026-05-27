13f6b1f7d21f4115d9dde7b12d1a0dda9ab48eb5
JupiterTav <wesleytavaresdev@outlook.com>
2026-05-27 15:53:06 -0300

    main.py -> to-do[0]

---
+++ src/main.py
@@ -0,0 +1,5 @@
+#TODO: classe campo_texto para leitura do texto (linhas) do usuario (posteriormente parte da GUI)
+#TODO: classe mixer (GerenciadorDeMusica) -> chama gerenciador_vozes e gerenciador_midi além de outras tarefas operacionais
+#TODO: Escrever a classe track (antiga voz) 
+#TODO:  - Parametros e metodos associados aos parametros
+#TODO:  - Metodos associados as tracks do midi (tocar nota, mudar instrumento, silencio) 
