eb628dad86636c7cf38880e9f4383c95e02a3035
JupiterTav <wesleytavaresdev@outlook.com>
2026-05-28 20:56:53 -0300

    EspecMidi

---
+++ src/espec_midi.py
@@ -0,0 +1,29 @@
+# TODO: Serve como o intepretador (branch prototipo). Deve ser a classe pai do
+# TODO: Gerenciador Midi e eventos midi.
+
+
+class EspecMidi:
+    def __init__(self):
+        self.__notas_midi = {
+            'A': 21,   # La (A0 = MIDI 21)
+            'B': 23,   # Si (B0 = MIDI 23)
+            'C': 12,   # Do (C0 = MIDI 12)
+            'D': 14,   # Re (D0 = MIDI 14)
+            'E': 16,   # Mi (E0 = MIDI 16)
+            'F': 17,   # Fa (F0 = MIDI 17)
+            'G': 19,   # Sol (G0 = MIDI 19)
+            'H': 22,   # Si Bemol (Bb0 = MIDI 22)
+            'M': 27
+        }
+
+        self.__gm_intruments = {
+                '!': 21,
+                'O': 109,
+                'o': 109,
+                'U': 109,
+                'u': 109,
+                'I': 109,
+                'i': 109,
+                ',': 19,
+                ';': 14
+        }
