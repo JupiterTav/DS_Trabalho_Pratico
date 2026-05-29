3d7a9757e45678111be711c919103a17d8454659
JupiterTav <wesleytavaresdev@outlook.com>
2026-05-29 20:40:56 -0300

    track (midi file)

--- src/main.py
+++ src/main.py
@@ -9,13 +9,18 @@ from gerador_vozes import GeradorVozes
 from gerenciador_midi import GerenciadorMidi
 
 
-texto = CampoTexto()
-texto.le_texto()
+def main():
+    texto = CampoTexto()
+    texto.le_texto()
 
-gerador_vozes = GeradorVozes()
-vozes = gerador_vozes.gerar_vozes(texto)
-print(vozes)
+    gerador_vozes = GeradorVozes()
+    vozes = gerador_vozes.gerar_vozes(texto)
 
-gerenciador_arq = GerenciadorMidi()
+    gerenciador_arq = GerenciadorMidi()
+    gerenciador_arq.gerarArquivo("build/musica")
 
-gerenciador_arq.gerarArquivo("build/musica")
+    for i,voz in enumerate(vozes): 
+        track = gerenciador_arq.criaTrack(f'voz {i}')
+
+if __name__ == "__main__":
+    main()
