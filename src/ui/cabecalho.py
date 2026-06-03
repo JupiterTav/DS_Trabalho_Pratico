9a6eb12e0e7a08e9d85c0a47d100c98493ec6b58
JupiterTav <wesleytavaresdev@outlook.com>
2026-06-02 22:47:18 -0300

    download_button removed
    Funcionalidade acontece ao iniciar o processo de mixagem

--- src/ui/cabecalho.py
+++ src/ui/cabecalho.py
@@ -6,10 +6,5 @@ class Cabecalho(ctk.CTkFrame):
         
         self.grid_columnconfigure((0), weight=1)
 
-        self.upload_txt = ctk.CTkButton(self, width=30, height=30, text="↑", corner_radius=50)
+        self.upload_txt = ctk.CTkButton(self, width=30, height=30, text="↑", corner_radius=70, font=("Arial", 20, "bold"))
         self.upload_txt.grid(row=0, column=0, sticky="w", padx=(0, 10))
-        
-        self.download_button = ctk.CTkButton(self, width=30, height=30 ,text="↓", corner_radius=50)
-        self.download_button.grid(row=0, column=1, padx=(10, 0), sticky="e")
-
-        self.download_button.grid_anchor("ne")
