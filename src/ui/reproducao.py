65bd22bd443c28ccf33ff70c4ac80cea6983740d
JupiterTav <wesleytavaresdev@outlook.com>
2026-06-04 02:31:57 -0300

    melhor centralização dos botões

--- src/ui/reproducao.py
+++ src/ui/reproducao.py
@@ -7,15 +7,13 @@ class Reproducao(ctk.CTkFrame):
     def __init__(self, master, mixer: Mixer):
         super().__init__(master)
 
-        self.grid_columnconfigure((0, 1, 2), weight=1)
+        self.grid_columnconfigure((1,2), weight=1)
         self.grid_rowconfigure(0, weight=1)
         
-        self.pause_button = ctk.CTkButton(self, width=50, height=40, corner_radius=30, text="||", command=lambda: mixer.on_pause())
-        self.pause_button.grid(row=0, column=1, sticky="e")
+        self.play_button = ctk.CTkButton(self, width=45, height=40, corner_radius=25, text="▶", command=lambda: mixer.on_play())
+        self.play_button.grid(row=0, column=1, sticky="e")
 
-        self.play_button = ctk.CTkButton(self, width=50, height=40, corner_radius=30, text="▶", command=lambda: mixer.on_play())
-        self.play_button.grid(row=0, column=1, sticky="w", padx=75)
+        self.pause_button = ctk.CTkButton(self, width=45, height=40, corner_radius=25, text="||", command=lambda: mixer.on_pause())
+        self.pause_button.grid(row=0, column=2, sticky="w", padx=5)
+
         
-        self.volume = ctk.CTkSlider(self, width=170, height=10, corner_radius=30)
-        self.volume.grid(row=0, column=2, sticky="e")
-
